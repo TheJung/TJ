e4099e93585425f211ca2a94353fbd3652823136
Seania Twix <admin@ez.is>
2017-12-04 14:35:05 +0900

    first commit

---
+++ client/imports/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
+
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      // Home Page
+      {
+        path: '',
+        redirectTo: '/',
+        pathMatch: 'full'
+      },
+      // 404 Page
+      {
+        path: '**',
+        component: PageNotFoundComponent
+      }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
