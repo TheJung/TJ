e9fc1e4e899ff1563a8c4106cc959b7fe4708da0
Seania Twix <admin@ez.is>
2017-12-07 23:57:17 +0900

    암호화 코드 정리

--- server/imports/methods/hashing.ts
+++ server/imports/methods/hashing.ts
@@ -4,21 +4,17 @@ import { hash, argon2d, verify } from 'argon2';
 import { MeteorObservable } from 'meteor-rxjs';
 
 Meteor.methods({
-  'hashPassword': (password: string) => {
+  'password.hash': (password: string) => {
     const hashed_pw = hash('password', { type: argon2d });
     hashed_pw.then(a => { console.log(password, " : ", a); });
 
     return hashed_pw;
   },
 
-  'verifyPassword': async (raw_string: string) => {
+  'password.verify': async (raw_string: string) => {
     
-    const verifying = await verify(h1, raw_string);
+    const isValid = await verify('', raw_string);
 
-    if (verifying) {
-      console.log('correct');
-    } else {
-      console.log('incorrect');
-    }
+    return isValid;
   }
 })
