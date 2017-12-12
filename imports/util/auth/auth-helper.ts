fd38bddac246fc2568f14fc13947d9ef9c7d8aec
Seania Twix <admin@ez.is>
2017-12-12 20:49:30 +0900

    AuthHelper - 현재 유저 정보 획득 메소드 구현

--- imports/util/auth/auth-helper.ts
+++ imports/util/auth/auth-helper.ts
@@ -1,6 +1,13 @@
 import { LocalDatabase } from './../db/localdb-helper';
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+
+interface ClientSafeUserInfo {
+  _id: Mongo.ObjectID,
+  id: string,
+  nickname: string
+}
 
 export class AuthHelper {
   private isLoggedIn: Boolean = false;
@@ -11,10 +18,8 @@ export class AuthHelper {
    * @param id - 사용자 지정 가능한 ID 값입니다.
    * 
    * @param password - 평문의 비밀번호 입니다. 서버에서 암호화 해 비교합니다.
-   * 
-   * @param failed - 로그인에 실패했을 때 호출 되는 콜백 함수입니다. err 객체를 인자로 갖습니다.
    */
-  public async login(id: string, password: string, failed?: Function): Promise<string> {
+  public async login(id: string, password: string): Promise<string> {
     let request: Promise<string> = new Promise((resolve, reject) => {
       // id: string, password: string
       Meteor.call('user.login', id, password, (err, res) => {
@@ -98,6 +103,23 @@ export class AuthHelper {
     return state;
   }
 
+  public async currentUserInfo(): Promise<ClientSafeUserInfo> {
+    let db = new LocalDatabase('user');
+    let signed: any = await db.get('latest-signed');
+
+    let info: Promise<ClientSafeUserInfo> = new Promise((response, reject) => {
+      Meteor.call('user.info.me', signed.token, (err, res) => {
+        if (err !== undefined) {
+          reject(err);
+        }
+
+        response(res);
+      });
+    });
+
+    return info;
+  }
+  
   /**
    * logout - 로컬 데이터베이스의 저장된 토큰을 지웁니다.
    */
